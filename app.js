e59b5e1a852a6fb8b1c435ba1ec4f073ab82fc37
sajeev zacharias <sajeev.zacharias4@gmail.com>
2014-01-16 17:27:45 +0000

    Initialize repository

---
+++ app.js
@@ -0,0 +1,49 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+var dbUrl="library";
+var collections=["books"];
+var db=require("mongojs").connect(dbUrl,collections);
+
+
+app.get('/', routes.index);
+app.get("/books",function(req,res){
+	db.books.find({},function(err,books){
+		if(err) return;
+		var response={
+			books:books
+			};
+		res.json(response);
+	});
+});
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
