2b554e535f4b88c5741126b87ed12f29e40ecbda
sajeev zacharias <sajeev.zacharias4@gmail.com>
2014-01-17 15:17:42 +0000

    Fourth  commit Backbone rendered into browser

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,3 +1,10 @@
 $(function(){
+	var collection=new BookCollection();
+	collection.fetch({
+		success: function(data){
+			var view =new BookCollectionView({collection:data});
+			$("body").append(view.render().el);
+		}
+	});
 	
 });
