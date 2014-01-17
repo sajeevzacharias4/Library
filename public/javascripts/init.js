faf23cdf2a158ce493387d9b9f61f3786f53cf1f
sajeev zacharias <sajeev.zacharias4@gmail.com>
2014-01-17 16:19:05 +0000

    Sixth  commit Backbone using router

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,10 +1,5 @@
 $(function(){
-	var collection=new BookCollection();
-	collection.fetch({
-		success: function(data){
-			var view =new BookCollectionView({collection:data});
-			$("body").append(view.render().el);
-		}
-	});
+	new AppRouter();
+	Backbone.history.start()
 	
 });
